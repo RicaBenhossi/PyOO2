2e6f330b3da24ee334a999a780f27e07c81938f4
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2022-03-01 15:57:55 -0300

    Duck Typing, composition and when not use heritage.

--- modelo.py
+++ modelo.py
@@ -1,8 +1,15 @@
+import reprlib
+from reprlib import repr
+
+
 class Programa:
     def __init__(self, nome, ano_lancamento):
         self._nome = nome.title()   # If we use __, the attribute is set as private and will not be passed to children
         self._ano = ano_lancamento  # by heritage. To avoid it we use only one _ just to sinalize to programmers that
         self._likes = 0             # this attribute is protected. BUT this is only by convention.
+
+    def __str__(self):
+        return f"{self.nome}\n\tLançamento: {self.ano_lancamento}\n\t{self.likes} Likes"
 
     @property
     def likes(self):
@@ -29,6 +36,10 @@ class Filme(Programa):
         super().__init__(nome, ano_lancamento)  # super allows you to access the mother class from where you inherited
         self._duracao = duracao
 
+    def __str__(self):
+        return f"{self.nome}\n\tLançamento: {self.ano_lancamento}\n\tDuração: {self.duracao} min\n\t" \
+               f"{self.likes} Likes"
+
     @property
     def duracao(self):
         return self._duracao
@@ -39,16 +50,61 @@ class Serie(Programa):
         super().__init__(nome, ano_lancamento)
         self._temporadas = temporadas
 
+    def __str__(self):
+        return f"{self.nome}\n\tLamçamento: {self.ano_lancamento}\n\tTemporada(s): {self.temporadas}\n\t" \
+               f"{self.likes} Likes"
+
     @property
     def temporadas(self):
         return self._temporadas
 
 
-filme = Filme('vingadores - guerra infinita', 2018, 160)
-filme.dar_likes()
-print(f'{filme.nome} ({filme.ano_lancamento}): {filme.likes} curtidas.')
+class Playlitst:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
 
-serie = Serie('atlanta', 2018, 2)
-serie.dar_likes()
-serie.dar_likes()
-print(f'{serie.nome} ({serie.ano_lancamento}): {serie.likes} curtidas.')
+    # This dunder method is what make a list iterable. When we implement it here, we gain list behaviours without set
+    # this class as a LIST. It calls DUCK TYPING (I don´t wnat know if it is a duck.I just want it "quacks" like one)
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    # This dunder method allow us to use LEN in our object.
+    def __len__(self):
+        return len(self._programas)
+
+    @property
+    def tamanho(self):
+        return len(self.programas)
+
+
+def gera_like(likes: int, programa_like):
+    x = 0
+    while x < likes:
+        programa_like.dar_likes()
+        x += 1
+
+
+filme = Filme("vingadores - guerra infinita", 2018, 160)
+gera_like(3, filme)
+
+serie1 = Serie("atlanta", 2018, 2)
+gera_like(5, serie1)
+
+serie2 = Serie('todo mundo odeia o chris', 2009, 7)
+gera_like(10, serie2)
+
+serie3 = Serie("ghost", 2021, 1)
+gera_like(20, serie3)
+
+lista = [serie2, serie3, serie1]
+minha_playlist = Playlitst("Ver já!", lista)
+
+# After implement __len__ in class Playlist we gain the ability to use LEN() in our object
+print(f"Sua lista possui {len(minha_playlist)} programa(os).")
+
+# After implement __getitem__ in class Playlist we gain all the particularity of a list, BUT without using heritage.
+# This is called DUCK TYPING. That means that I don´t want my class to become a list (trough heritage), I want it just
+# have some behaviours of a list, like be iterable, allow me to us "IN" and "FOR IN".
+for programa in minha_playlist:
+    print(programa)
